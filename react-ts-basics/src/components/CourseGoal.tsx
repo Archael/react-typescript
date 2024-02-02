f962d8005c0c142c3a3d91a6f02cbf764ad982bf
Adam Evans <adam3van5@gmail.com>
2024-02-02 12:55:26 +0900

    create component

---
+++ react-ts-basics/src/components/CourseGoal.tsx
@@ -0,0 +1,18 @@
+export default function CourseGoal({
+  // The props are destructured here using the {} syntax, standard object destructuring in javascript
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) {
+  return (
+    <article>
+      <div>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+      <button>Delete</button>
+    </article>
+  );
+}
